105a306d2d4a740bc21b47394d09dbf83296a7b6
SaraMarhoum <57989282+SaraMarhoum@users.noreply.github.com>
2021-06-18 17:51:38 +0100

    Add project

---
+++ CalculatorApp/CalculatorAppTests/CalculatorTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using CalculatorConsole;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorAppTests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        private Calculator calculator;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            calculator = new Calculator();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+        }
+
+        [TestMethod]
+        public void Add()
+        {
+            Assert.AreEqual(15, calculator.Add(10, 5));
+
+        }
+
+        [TestMethod]
+        public void Subtract()
+        {
+            Assert.AreEqual(0, calculator.Subtract(1, 1));
+
+        }
+
+        [TestMethod]
+        public void Multiply()
+        {
+            Assert.AreEqual(6, calculator.Multiply(3, 2));
+
+        }
+
+        [TestMethod]
+        public void Divide()
+        {
+            Assert.AreEqual(4, calculator.Divide(8, 2));
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DivideException()
+        {
+            calculator.Divide(2, 0);
+        }
+
+    }
+}
---
+++ CalculatorApp/CalculatorAppTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorAppTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorAppTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4e2f7825-7ea4-42f3-b3b7-51c40a4328e3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
